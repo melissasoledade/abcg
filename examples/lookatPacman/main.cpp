63c41f539d6a8ae833097013df4128d2c7d63ecc
melissasoledade <melissa.perrone@quintoandar.com.br>
2021-11-16 21:14:59 -0300

    main titulo

--- examples/lookatPacman/main.cpp
+++ examples/lookatPacman/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
     auto window{std::make_unique<OpenGLWindow>()};
     window->setOpenGLSettings({.samples = 4});
     window->setWindowSettings(
-        {.width = 600, .height = 600, .title = "LookAt Camera"});
+        {.width = 600, .height = 600, .title = "Pacman"});
 
     app.run(std::move(window));
   } catch (const abcg::Exception &exception) {
